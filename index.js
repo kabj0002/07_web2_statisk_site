9c644949dc3c067842d322cf3f7e70ad949005e2
Katinka H. Bjerke <kabj0002@stud.kea.dk>
2024-09-01 14:47:58 +0200

    styling statisk site

---
+++ index.js
@@ -0,0 +1,42 @@
+// Moving text 1, Codepen
+
+(function () {
+  var welcomeAnimationId,
+    parentAnimationElementWidth,
+    startPosAnimationElement,
+    posAnimationElement,
+    animationElement;
+
+  function setWelcomeElement() {
+    animationElement = document.querySelector(".welcome-text");
+    parentAnimationElementWidth =
+      animationElement.parentElement.parentElement.offsetWidth;
+    startPosAnimationElement = -animationElement.offsetWidth;
+    posAnimationElement = startPosAnimationElement;
+    animationElement.style.right = posAnimationElement + "px";
+  }
+
+  function moveWelcomeElement() {
+    posAnimationElement += 2;
+    animationElement.style.right = posAnimationElement + "px";
+    if (posAnimationElement > parentAnimationElementWidth)
+      posAnimationElement = startPosAnimationElement;
+    portfolioAnimationId = requestAnimationFrame(moveWelcomeElement);
+  }
+
+  setWelcomeElement();
+  moveWelcomeElement();
+  window.addEventListener("resize", refreshAnimation, false);
+
+  var resizeTimeout;
+  function refreshAnimation() {
+    if (!resizeTimeout) {
+      resizeTimeout = setTimeout(function () {
+        resizeTimeout = null;
+        window.cancelAnimationFrame(welcomeAnimationId);
+        setWelcomeElement();
+        moveWelcomeElement();
+      }, 100);
+    }
+  }
+})();
