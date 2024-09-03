be79af7c9f93621911f66f6172ef1ebb5d855d9b
Katinka H. Bjerke <kabj0002@stud.kea.dk>
2024-09-03 22:20:17 +0200

    styling af site

--- category.js
+++ category.js
@@ -4,20 +4,21 @@ const params = new URLSearchParams(window.location.search);
 const category = params.get("category");
 let url = undefined;
 
-fetch("https://kea-alt-del.dk/t7/api/categories")
-  .then((response) => response.json())
-  .then((categories) => {
-    categories.forEach((category) => {
-      categoryList.innerHTML += `<li><a href="kategori.html?category=${category.category}">${category.category}</a></li>`;
-    });
-    //categoryList.innerHTML = `<li>${categories}</li>`;
-  });
+//Gjort i undervisning
+// fetch("https://kea-alt-del.dk/t7/api/categories")
+//   .then((response) => response.json())
+//   .then((categories) => {
+//     categories.forEach((category) => {
+//       categoryList.innerHTML += `<li><a href="kategori.html?category=${category.category}">${category.category}</a></li>`;
+//     });
+//     //categoryList.innerHTML = `<li>${categories}</li>`;
+//   });
 
-if (params.has("category")) {
-  url = "https://kea-alt-del.dk/t7/api/products?category=" + category;
-} else {
-  url = "https://kea-alt-del.dk/t7/api/products";
-}
+// if (params.has("category")) {
+//   url = "https://kea-alt-del.dk/t7/api/products?category=" + category;
+// } else {
+//   url = "https://kea-alt-del.dk/t7/api/products";
+// }
 
 //Video tutorial
 fetch("https://kea-alt-del.dk/t7/api/categories")
